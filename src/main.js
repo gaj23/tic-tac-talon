b6f1e454ab0ec03dec802f158e7f866822764610
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2020-11-07 10:58:39 -0500

    Expand manageGamePlay functionality

--- src/main.js
+++ src/main.js
@@ -1,20 +1,31 @@
+var game = new Game();
+// where should this global variable exist? --> inside of my game.js, but still not sure how to pass in my two players as parameters?
+
 var gameboard = document.querySelector('.gameboard');
 var restartButton = document.querySelector('button');
+var header = document.querySelector('#player-turn')
 
 gameboard.addEventListener('click', manageGamePlay);
-restartButton.addEventListener('click', restartGame)
+restartButton.addEventListener('click', restartGame);
 
 function manageGamePlay(event) {
   var square = event.target
-  if (square.className === 'square') {
-    console.log('lolwut');
+  if (square.innerHTML !== '') {
+    for (var i = 0; i < game.squareIDs.length; i++) {
+      if (square.id === game.squareIDs[i] && header.innerText.includes('Turkey')) {
+        square.innerHTML = `<img src=${game.player1.imageSrc} alt=${game.player1.alt} class="board-image" id=${game.player1.id}>`
+        game.determineTurn();
+      } else {
+        game.determineTurn();
+      }
+    }
+
   }
 }
 
-function restartGame() {
-  alert('This is the last thing you\'ll do!');
-}
+function toggleToken() {}
 
+//why is my image not toggling?
 
 
 
@@ -25,3 +36,12 @@ function restartGame() {
 //inside of this function the three methods inside of game.js will fire: determineTurn, checkBoard, clearBoard
 
 //in conjunction with determineTurn, I feel as if if will be necessary to thus keep the change of the innerHTML of our header in another function.
+
+function restartGame() {
+  alert('Wow! You pushed the button!');
+  localStorage.clear();
+  var eagleScore = document.querySelector('.eagle-score');
+  var turkeyScore = document.querySelector('.turkey-score');
+  eagleScore.innerText = '0';
+  turkeyScore.innerText = '0';
+}
