9f0bc5bbe013ba30e7988c64acda1f7b6c8192f3
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2020-11-07 17:29:50 -0500

    Refactor toggleToken  to utilize game.currentPlayer properties

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 var game = new Game();
-// where should this global variable exist? --> inside of my game.js, but still not sure how to pass in my two players as parameters?
 
 var gameboard = document.querySelector('.gameboard');
 var restartButton = document.querySelector('button');
@@ -14,26 +13,20 @@ function manageGamePlay(event) {
 }
 
 function manageSquares(square) {
+  console.log('current player', game.currentPlayer)
   for (var i = 0; i < game.squareIDs.length; i++) {
     if (square.id === game.squareIDs[i]) {
-      game.determineTurn();
       toggleToken(square);
+      game.determineTurn();
       square.classList.add('disabled');
+      console.log('current player', game.currentPlayer)
     }
   }
 }
 
-//Can I refactor using parameters/passing in game.currentPlayer? & interpolating the src, alt, and id?
-//yes square.src = game.currentPlayer.imageSrc, ect...
-//or no currentPlayer and just use game.player1 or game.player2 properties
 function toggleToken(square) {
-  if (game.playCount === 0) {
-    square.innerHTML = '<img src="assets/bald-eagle.png" alt="bald eagle cartoon" class="board-image" id="player-eagle">';
-    header.innerText = "Turkey's Turn";
-  } else if (game.playCount === 1) {
-    square.innerHTML = '<img src="assets/turkey.png" alt="turkey cartoon" class="board-image" id="player-turkey">';
-    header.innerText = "Bald Eagle's turn";
-  }
+  header.innerText = `${game.currentPlayer.header}'s Turn!`
+  square.innerHTML = `<img src="${game.currentPlayer.imageSrc}" alt="${game.currentPlayer.alt}" class="board-image" id="${game.currentPlayer.id}">`;
 }
 
 
