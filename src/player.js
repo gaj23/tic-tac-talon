b27c082e9f668ec1c089b43001a8071693035ed2
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2020-11-07 17:26:05 -0500

    Added a property of header

--- src/player.js
+++ src/player.js
@@ -3,6 +3,7 @@ class Player {
     this.imageSrc = playerObj.imageSrc;
     this.id = playerObj.id;
     this.alt = playerObj.alt;
+    this.header = playerObj.header
     this.wins = 0;
   }
   saveWinsToStorage() {
