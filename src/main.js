424a1bf48adffebcd6468cdbd5add80d2aa30322
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2020-11-07 16:15:52 -0500

    Debugged manageGamePlay by separating logic into another function

--- src/main.js
+++ src/main.js
@@ -9,46 +9,33 @@ gameboard.addEventListener('click', manageGamePlay);
 restartButton.addEventListener('click', restartGame);
 
 function manageGamePlay(event) {
-  game.determineTurn();
   var square = event.target;
-  if (square.innerHTML !== '') {
-    for (var i = 0; i < game.squareIDs.length; i++) {
-      if (square.id === game.squareIDs[i]) {
-        toggleToken(square);
-        toggleHeader();
-      }
+  manageSquares(square);
+}
+
+function manageSquares(square) {
+  for (var i = 0; i < game.squareIDs.length; i++) {
+    if (square.id === game.squareIDs[i]) {
+      game.determineTurn();
+      toggleToken(square);
+      square.classList.add('disabled');
     }
   }
 }
 
-//can I refactor by passing in parameters instead? aka currentPlayer?
-function toggleHeader() {
-  if (header.innerText.includes('Turkey')) {
-    header.innerText = "Bald Eagle's turn";
-  } else {
-    header.innerText = "Turkey's Turn";
-  }
-}
-
 //Can I refactor using parameters/passing in game.currentPlayer? & interpolating the src, alt, and id?
+//yes square.src = game.currentPlayer.imageSrc, ect...
+//or no currentPlayer and just use game.player1 or game.player2 properties
 function toggleToken(square) {
   if (game.playCount === 0) {
     square.innerHTML = '<img src="assets/bald-eagle.png" alt="bald eagle cartoon" class="board-image" id="player-eagle">';
-  } else {
+    header.innerText = "Turkey's Turn";
+  } else if (game.playCount === 1) {
     square.innerHTML = '<img src="assets/turkey.png" alt="turkey cartoon" class="board-image" id="player-turkey">';
+    header.innerText = "Bald Eagle's turn";
   }
 }
 
-
-
-
-//this manages the entirety of the game play
-//it asks three questions: who's turn is it? Who's token is being placed? And has the winning scienarios been met?
-//and it fires all three of the functions/questions for every click!
-
-//inside of this function the three methods inside of game.js will fire: determineTurn, checkBoard, clearBoard
-
-//in conjunction with determineTurn, I feel as if if will be necessary to thus keep the change of the innerHTML of our header in another function.
 
 function restartGame() {
   alert('Wow! You pushed the button!');
