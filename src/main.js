2b402054d70aeca1515f843af0f4261d1b3ca3cb
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2020-11-08 18:45:56 -0500

    Declare checkWinStatus and checkHorizontal functionss

--- src/main.js
+++ src/main.js
@@ -5,19 +5,18 @@ var restartButton = document.querySelector('button');
 var header = document.querySelector('#player-turn');
 
 gameboard.addEventListener('click', manageGamePlay);
-header.addEventListener('click', toggleToken);;
 restartButton.addEventListener('click', restartGame);
 
 function manageGamePlay(event) {
   var square = event.target;
   manageSquares(square);
+  checkWinStatus();
 }
 
 function manageSquares(square) {
   for (var i = 0; i < game.squareIDs.length; i++) {
     if (square.id === game.squareIDs[i]) {
       toggleToken(square);
-      square.classList.add('disabled');
       game.determineTurn();
       header.innerText = `${game.currentPlayer.header}'s Turn!`
     }
@@ -26,7 +25,41 @@ function manageSquares(square) {
 
 function toggleToken(square) {
   square.innerHTML = `<img src="${game.currentPlayer.imageSrc}" alt="${game.currentPlayer.alt}" class="board-image" id="${game.currentPlayer.id}">`;
+  var tokenID = game.currentPlayer.id;
+  square.classList.add(tokenID);
+  square.classList.add('disabled');
 }
+
+function checkWinStatus() {
+  checkHorizontal();
+  // checkVertical();
+  // checkDiagonal();
+}
+
+function checkHorizontal() {
+  var sq1 = document.getElementById('one');
+  var sq2 = document.getElementById('two');
+  var sq3 = document.getElementById('three');
+  var sq4 = document.getElementById('four')
+  var sq5 = document.getElementById('five')
+  var sq6 = document.getElementById('six')
+  var sq7 = document.getElementById('seven')
+  var sq7 = document.getElementById('eight')
+  var sq9 = document.getElementById('nine')
+  if (sq1.classList.contains(`${game.currentPlayer.id}`) && sq2.classList.contains(`${game.currentPlayer.id}`) && sq3.classList.contains(`${game.currentPlayer.id}`)) {
+    console.log('please');
+    // gameboard.classList.add('disabled');
+    // header.innerText = 'Turkey Wins!'
+    //why is it coming up undefined?
+  } else if (sq4.classList.contains(`${game.currentPlayer.id}`) && sq5.classList.contains(`${game.currentPlayer.id}`) && sq6.classList.contains(`${game.currentPlayer.id}`)) {
+    console.log('please')
+  } else if (sq7.classList.contains(`${game.currentPlayer.id}`) && sq8.classList.contains(`${game.currentPlayer.id}`) && sq9.classList.contains(`${game.currentPlayer.id}`)) {
+    console.log('please')
+  }
+  //currentPlayer is lagging
+  //why can't I put the getElementById in the checkWinStatus?
+}
+
 
 
 function restartGame() {
