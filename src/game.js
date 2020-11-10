a62a721e031f892bfa8c8c19a839c41c41ea6c9a
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2020-11-10 11:27:47 -0500

    Move checkWinStatus into game class by utilizing an array of objects

--- src/game.js
+++ src/game.js
@@ -12,18 +12,44 @@ class Game {
       alt: 'bald eagle cartoon',
       header: 'Bald Eagle',
     });
-    this.squareIDs = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    this.squareIDs = [{
+      id: "one",
+      selectedBy: null
+    }, {
+      id: "two",
+      selectedBy: null
+    }, {
+      id: "three",
+      selectedBy: null
+    }, {
+      id: "four",
+      selectedBy: null
+    }, {
+      id: "five",
+      selectedBy: null
+    }, {
+      id: "six",
+      selectedBy: null
+    }, {
+      id: "seven",
+      selectedBy: null
+    }, {
+      id: "eight",
+      selectedBy: null
+    }, {
+      id: "nine",
+      selectedBy: null
+    }]
     this.currentPlayer = this.player1;
     this.win = false;
+    this.playCount = 0;
   }
-
   updateStoredScore() {
     var turkeyWins = JSON.stringify(this.player1.score);
     localStorage.setItem('turkeyScore', turkeyWins);
     var eagleWins = JSON.stringify(this.player2.score);
     localStorage.setItem('eagleScore', eagleWins);
   }
-
   updatePlayerScore() {
     if (this.currentPlayer === this.player1) {
       var updateTurkey = JSON.parse(localStorage.getItem('turkeyScore'));
@@ -41,12 +67,64 @@ class Game {
       this.updateStoredScore();
     }
   }
-
   determineTurn() {
     if (this.currentPlayer === this.player2) {
       this.currentPlayer = this.player1;
     } else {
       this.currentPlayer = this.player2;
+    }
+  }
+  // updateStoredPlayCount() {
+  //   var playCount = JSON.stringify(this.playCount);
+  //   localStorage.setItem('playCount', playCount);
+  // }
+  //
+  // increasePlayCount() {
+  //   var updateCount = JSON.parse(localStorage.getItem('playCount'));
+  //   this.playCount = updateCount;
+  //   this.playCount += 1;
+  //   this.updateStoredPlayCount();
+  // }
+
+  updateSquareIDs(squareID, tokenID) {
+    for (var i = 0; i < this.squareIDs.length; i++) {
+      if (this.squareIDs[i].id === squareID) {
+        this.squareIDs[i].selectedBy = tokenID;
+      }
+    }
+  }
+  checkHorizontal() {
+    if (this.squareIDs[0].selectedBy === this.currentPlayer.id && this.squareIDs[1].selectedBy === this.currentPlayer.id && this.squareIDs[2].selectedBy === this.currentPlayer.id) {
+      this.win = true;
+    } else if (this.squareIDs[3].selectedBy === this.currentPlayer.id && this.squareIDs[4].selectedBy === this.currentPlayer.id && this.squareIDs[5].selectedBy === this.currentPlayer.id) {
+      this.win = true;
+    } else if (this.squareIDs[6].selectedBy === this.currentPlayer.id && this.squareIDs[7].selectedBy === this.currentPlayer.id && this.squareIDs[8].selectedBy === this.currentPlayer.id) {
+      this.win = true;
+    }
+  }
+  checkVertical() {
+    if (this.squareIDs[0].selectedBy === this.currentPlayer.id && this.squareIDs[3].selectedBy === this.currentPlayer.id && this.squareIDs[6].selectedBy === this.currentPlayer.id) {
+      this.win = true;
+    } else if (this.squareIDs[1].selectedBy === this.currentPlayer.id && this.squareIDs[4].selectedBy === this.currentPlayer.id && this.squareIDs[7].selectedBy === this.currentPlayer.id) {
+      this.win = true;
+    } else if (this.squareIDs[2].selectedBy === this.currentPlayer.id && this.squareIDs[5].selectedBy === this.currentPlayer.id && this.squareIDs[8].selectedBy === this.currentPlayer.id) {
+      this.win = true;
+    }
+  }
+  checkDiagonal() {
+    if (this.squareIDs[0].selectedBy === this.currentPlayer.id && this.squareIDs[4].selectedBy === this.currentPlayer.id && this.squareIDs[8].selectedBy === this.currentPlayer.id) {
+      this.win = true;
+    } else if (this.squareIDs[2].selectedBy === this.currentPlayer.id && this.squareIDs[4].selectedBy === this.currentPlayer.id && this.squareIDs[6].selectedBy === this.currentPlayer.id) {
+      this.win = true;
+    }
+  }
+  checkTie() {
+    var squareCount = 0;
+    for (var i = 0; i < this.squareIDs.length; i++) {
+      if (this.squareIDs[i].selectedBy !== null) {
+        squareCount += 1;
+      }
+      if (squareCount === 9) this.win = null;
     }
   }
 }
