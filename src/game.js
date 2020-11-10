862eb6c6a06a49850ca3884f74f7e08afc7b28ed
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2020-11-09 20:49:35 -0500

    Move clearBoard function into main.js and update  properties of players

--- src/game.js
+++ src/game.js
@@ -4,46 +4,43 @@ class Game {
       imageSrc: 'assets/turkey.png',
       id: 'turkey',
       alt: 'turkey cartoon',
-      header: 'Turkey'
+      header: 'Turkey',
     });
     this.player2 = new Player({
       imageSrc: 'assets/bald-eagle.png',
       id: 'bald-eagle',
       alt: 'bald eagle cartoon',
-      header: 'Bald Eagle'
+      header: 'Bald Eagle',
     });
     this.squareIDs = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     this.currentPlayer = this.player1;
     this.win = false;
-    this.playCount = 0; //needed?
   }
 
   updateStoredScore() {
-    var turkeyWins = JSON.stringify(this.player1.wins);
+    var turkeyWins = JSON.stringify(this.player1.score);
     localStorage.setItem('turkeyScore', turkeyWins);
-    var eagleWins = JSON.stringify(this.player2.wins);
+    var eagleWins = JSON.stringify(this.player2.score);
     localStorage.setItem('eagleScore', eagleWins);
   }
 
   updatePlayerScore() {
     if (this.currentPlayer === this.player1) {
       var updateTurkey = JSON.parse(localStorage.getItem('turkeyScore'));
-      this.player1.wins = updateTurkey;
-      this.player1.wins += 1;
+      this.player1.score = updateTurkey;
+      this.player1.score += 1;
       var eagle = JSON.parse(localStorage.getItem('eagleScore'));
-      this.player2.wins = eagle;
+      this.player2.score = eagle;
       this.updateStoredScore();
     } else {
       var updateEagle = JSON.parse(localStorage.getItem('eagleScore'));
-      this.player2.wins = updateEagle;
-      this.player2.wins += 1;
+      this.player2.score = updateEagle;
+      this.player2.score += 1;
       var turkey = JSON.parse(localStorage.getItem('turkeyScore'));
-      this.player1.wins = turkey;
+      this.player1.score = turkey;
       this.updateStoredScore();
     }
   }
-
-  //get data from localStorage, reassign that number to player1 & 2 and then increment that "new" number then store that new value back in.
 
   determineTurn() {
     if (this.currentPlayer === this.player2) {
@@ -52,16 +49,4 @@ class Game {
       this.currentPlayer = this.player2;
     }
   }
-
-  checkBoard() {
-    //Why do I need this?
-  }
-
-  clearBoard() {
-    setTimeout(function() {
-      window.location.reload();
-    }, 3000);
-  }
-  //is this technically DOM manipulation?
-
 }
